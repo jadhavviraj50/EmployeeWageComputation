0c5884dd2d672fc94d96dd0d79d58ffd52cf6fc4
Viraj <jadhavviraj50@gmail.com>
2022-04-19 23:27:37 +0530

    UC2_Program

--- EmployeeWageComputation/EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/EmployeeWageComputation/Program.cs
@@ -1,17 +1,23 @@
-﻿class Program
+﻿
+Console.WriteLine("Welcome to Employee Wage Computation Program!")
+namespace EmployeeWageComputation
 {
-    static void Main(string[] args)
+    class Program
     {
-        int IS_FULL_TIME = 1;
-        Random random = new Random();
-        int empCheck = random.Next(0, 2);
-        if (empCheck == IS_FULL_TIME)
+        static void Main(string[] args)
         {
-            Console.WriteLine("Employee is Present");
-        }
-        else
-        {
-            Console.WriteLine("Employee is Absent");
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+
         }
     }
 }
